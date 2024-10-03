0882053b2d55e10dd1f70ae18c81b27a79ba27c4
BryanVandenbossche <126100348+BryanVandenbossche@users.noreply.github.com>
2024-10-03 13:11:28 +0200

    Update sample-app.sh
    if statement mkdir tempdir

--- sample-app.sh
+++ sample-app.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euo pipefail
 
+if [ ! -d /vagrant/cicd-sample-app/tempdir/]; then
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
+fi
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
